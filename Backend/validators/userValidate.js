879f4b7a8a00c8ec049c970af50e806f008e69bb
Muskan Mujawar <muskanmujawar1729@gmail.com>
2026-03-24 13:07:47 +0530

    some file updated file updated

--- Backend/validators/userValidate.js
+++ Backend/validators/userValidate.js
@@ -1,33 +1,31 @@
-import yup, { Schema } from "yup"
+import * as yup from "yup"
 
 export const userSchema = yup.object({
-    //username 
-    username: yup
-        .string()
-        .trim()
-        .min(3, "Username must be atleast of 3 characters")
-        .required(),
+  username: yup
+    .string()
+    .trim()
+    .min(3, "Username must be at least 3 characters")
+    .required("Username is required"),
 
-    //yup for email
-    email: yup
-        .string()
-        .email("The email is not valid one")
-        .required(),
+  email: yup
+    .string()
+    .email("Invalid email")
+    .required("Email is required"),
 
-    //yup for password
-    password: yup
-        .string()
-        .min(4, "Password must be atleast 4 character")
-        .required()
-
-})
+  password: yup
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
 
 export const validateUser = (schema) => async (req, res, next) => {
-    try {
-        await schema.validate(req.body)
-        next()
-    } catch (err) {
-        return res.status(400).json({ errors: err.errors })
-
-    }
+  try {
+    await schema.validate(req.body, { abortEarly: false })
+    next()
+  } catch (err) {
+    return res.status(400).json({
+      success: false,
+      errors: err.errors
+    })
+  }
 }
